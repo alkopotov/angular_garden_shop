7e46eefb556aa433935f02bb574443ab09eaf40d
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-07-15 16:54:30 +0300

    Cart page desktop completed

--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -4,11 +4,13 @@ import { CartStorageService } from '../../services/cart-storage.service';
 import { Router } from '@angular/router';
 import { CartItemComponent } from '../../components/cart-item/cart-item.component';
 import { CurrencyPipe } from '@angular/common';
+import { LoggerViewedService } from '../../services/logger-viewed.service';
+import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [CartItemComponent, CurrencyPipe],
+  imports: [CartItemComponent, CurrencyPipe, ProductListComponent],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
@@ -18,6 +20,7 @@ export class CartPageComponent implements OnInit {
     private _dataSourceService: DataSourceService,
     public cartStorageService: CartStorageService,
     private _router: Router,
+    public loggerViewedService: LoggerViewedService
   ) { }
 
   ngOnInit(): void {
