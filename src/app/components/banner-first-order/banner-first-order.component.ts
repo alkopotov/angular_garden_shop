a87057a9b91e179e7259c2312ee99f88d0f83fb7
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-07-19 12:22:04 +0300

    Header adaptive

--- src/app/components/banner-first-order/banner-first-order.component.ts
+++ src/app/components/banner-first-order/banner-first-order.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DialogDispatcherService } from '../../services/dialog-dispatcher.service';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-banner-first-order',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './banner-first-order.component.html',
   styleUrl: './banner-first-order.component.css'
 })
-export class BannerFirstOrderComponent {
+export class BannerFirstOrderComponent implements OnInit {
 
-  constructor(private _dialogDispatcher: DialogDispatcherService) { }
+  public dataForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl(''),
+  });
 
-  public handleSubmit(e: Event): void {
-    e.preventDefault();
-    this._dialogDispatcher.openDialog('discount');
+  constructor(
+    private _dialogDispatcher: DialogDispatcherService,
+    private _formBuilder: FormBuilder,  
+  ) { }
+
+  ngOnInit(): void {
+    this.dataForm = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z ]+$')]],
+      phone: ['', [Validators.required, Validators.minLength(10), Validators.pattern('^[0-9]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+    })
   }
- 
+
+  public onSubmit(): void {
+    if (this.dataForm.invalid) {
+      console.log('Form is invalid');
+    } else {
+    console.log(this.dataForm.value);
+    this._dialogDispatcher.openDialog('discount');
+    }
+  }
 }
